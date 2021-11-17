499ba8b8b77fb5f05c821dd34f639b663408882d
Raghu Vamsi Kothapalli <raghuvamsikothapalli@gmail.com>
2021-11-16 20:08:03 -0600

    Simple sorting algorithm python code added

--- simple_sorting_algorithm.py
+++ simple_sorting_algorithm.py
@@ -1,1 +1,36 @@
-print("Simple Sorting Algorithm")
+# generate random integer values
+from random import seed
+from random import randint
+# seed random number generator
+seed(1)
+
+arr = []
+
+# User input for number of elements
+n = int(input("Enter number of elements: "))
+
+# Inputting values in array through user
+'''
+# For user input
+while n!=0:
+    arr.append(int(input("Enter elements: ")))
+    n-=1
+'''
+
+# For random input
+for _ in range(n):
+    arr.append(randint(0, n))
+
+# Unsorted Array
+print("Unsorted Array: ", arr)
+
+# Sorting Array in Ascending Order
+for i in range(0, n):
+    j = i+1
+    for j in range(0, n):
+        if arr[j] > arr[i]:
+            temp = arr[i]
+            arr[i] = arr[j]
+            arr[j] = temp
+
+print("Sorted Array: ", arr)
